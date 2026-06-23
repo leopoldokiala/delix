db15325632b42967b09737902f4d032f8b912d8a
Leopoldo Kiala <leopoldostrong7@gmail.com>
2026-06-23 17:31:52 +0100

    feat: Refatoração Home_Screenr

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:cardapio_online/providers/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import './components/categories_button.dart';
-import 'components/app_drawer.dart';
-import './components/product_grid.dart';
+import './screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,107 +34,6 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         home: Home(),
-      ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: Theme.of(context).colorScheme.secondary,
-        ),
-        title: Text(
-          'Cardápio Online',
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.secondary,
-            fontSize: 24,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-          ),
-        ],
-      ),
-      drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: .start,
-          crossAxisAlignment: .start,
-          children: [
-            Text(
-              'Bem vindo, Usuário',
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.black87,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hint: Text('Buscar pratos ou bebidas'),
-                hintStyle: TextStyle(fontSize: 12),
-                prefixIcon: Icon(Icons.search),
-                // contentPadding: EdgeInsets.symmetric(vertical: 16),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.transparent),
-                ),
-                filled: true,
-                fillColor: Colors.grey.shade200,
-              ),
-            ),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: .spaceBetween,
-              children: [
-                CategoriesButton('Todos'),
-                CategoriesButton('Alimentos'),
-                CategoriesButton('Refrigerantes'),
-              ],
-            ),
-            Expanded(child: ProductGrid()),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        elevation: 5,
-        selectedItemColor: Colors.brown,
-        unselectedItemColor: Colors.black,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'início'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Meus Pedidos',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Meu Carrinho',
-          ),
-        ],
       ),
     );
   }
