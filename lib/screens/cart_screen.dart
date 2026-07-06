bafe9a8b212d9860ae337d838481bf04db721fdf
Leopoldo Kiala <leopoldostrong7@gmail.com>
2026-07-06 13:58:08 +0100

    Feat: Componente Carrinho

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of<Cart>(context);
     return Scaffold(
       appBar: AppBar(
         iconTheme: Theme.of(context).appBarTheme.iconTheme,
@@ -15,6 +18,41 @@ class CartScreen extends StatelessWidget {
             fontSize: 24,
           ),
         ),
+      ),
+
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text('Total', style: TextStyle(fontSize: 20)),
+                  SizedBox(width: 10),
+                  Chip(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    label: Text(
+                      'R\$ ${cart.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'COMPRAR',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
