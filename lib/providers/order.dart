db4bfa21c9ee30c1cf1bc680c24f28c2f2c39988
Leopoldo Kiala <leopoldostrong7@gmail.com>
2026-07-09 15:07:41 +0100

    feat: Criando Provider para pedido

---
+++ lib/providers/order.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './cart.dart';
+
+class Order {
+  final String id;
+  final double total;
+  final List<CartItem> products;
+  final DateTime date;
+
+  Order({
+    required this.id,
+    required this.total,
+    required this.products,
+    required this.date,
+  });
+}
+
+class Orders with ChangeNotifier {
+  final List<Order> _orders = [];
+
+  List<Order> get orders {
+    return [..._orders];
+  }
+}
