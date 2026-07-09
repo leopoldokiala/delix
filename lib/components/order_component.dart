e6a66b8801b6156ab35131e67ead8b0500acccfc
Leopoldo Kiala <leopoldostrong7@gmail.com>
2026-07-09 17:27:37 +0100

    Feat: Componente Pedidos

--- lib/components/order_component.dart
+++ lib/components/order_component.dart
@@ -2,39 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/order.dart';
 
-class OrderComponent extends StatelessWidget {
+class OrderComponent extends StatefulWidget {
   final Order order;
   const OrderComponent(this.order, {super.key});
 
   @override
+  State<OrderComponent> createState() => _OrderComponentState();
+}
+
+class _OrderComponentState extends State<OrderComponent> {
+  bool _expanded = false;
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: ListTile(
-        title: Row(
-          mainAxisAlignment: .spaceBetween,
-          children: [
-            Expanded(
-              child: Text(
-                '#ORD-${order.id}',
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.secondary,
+      child: Column(
+        children: [
+          ListTile(
+            title: Row(
+              mainAxisAlignment: .spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    '#ORD-${widget.order.id}',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
                 ),
+                Text(
+                  overflow: TextOverflow.ellipsis,
+                  '${widget.order.total.toString()} Kz',
+                  style: TextStyle(fontWeight: .bold, fontSize: 18),
+                ),
+              ],
+            ),
+            subtitle: Text(
+              DateFormat('dd/MM/yyy hh:mm').format(widget.order.date),
+              style: TextStyle(),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: _expanded
+                  ? Icon(Icons.expand_less_outlined)
+                  : Icon(Icons.expand_more),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: (widget.order.products.length * 25) + 10,
+              width: double.infinity,
+
+              child: ListView(
+                children: widget.order.products.map((product) {
+                  return Row(
+                    mainAxisAlignment: .spaceBetween,
+                    children: [
+                      Text(
+                        product.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${product.quantity} x ${product.price}',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  );
+                }).toList(),
               ),
             ),
-            Text(
-              overflow: TextOverflow.ellipsis,
-              '${order.total.toString()} Kz',
-              style: TextStyle(fontWeight: .bold, fontSize: 16),
-            ),
-          ],
-        ),
-        subtitle: Text(
-          DateFormat('dd/MM/yyy hh:mm').format(order.date),
-          style: TextStyle(),
-        ),
-        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
+        ],
       ),
     );
   }
