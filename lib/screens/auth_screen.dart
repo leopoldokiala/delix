32a759a0a1da97c06035192862f3683f4a6365ca
Leopoldo Kiala <leopoldostrong7@gmail.com>
2026-09-10 19:18:34 +0100

    feat: Tela de autenticação 2

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -12,10 +12,29 @@ class AuthScreen extends StatelessWidget {
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [Color(0xffb21029), Color(0xff3f050f)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
             ),
           ),
-          Container(width: double.infinity, child: Column()),
+          Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: .center,
+              crossAxisAlignment: .center,
+              children: [
+                Icon(Icons.restaurant, size: 45, color: Colors.white),
+                Text(
+                  'Délix',
+                  style: TextStyle(
+                    fontFamily: 'Anton',
+                    color: Colors.white,
+                    fontSize: 45,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
